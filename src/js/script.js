4d60e57bc01316ade7d2cc97bb28981ccb95a6d2
Adam Millington <infinitecookiehd@gmail.com>
2018-11-03 14:25:02 +0000

    changed to L2

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-const url = "https://www.footballwebpages.co.uk/vidiprinter.json?comp=3";
+const url = "https://www.footballwebpages.co.uk/vidiprinter.json?comp=4";
 const outputDiv = document.getElementById('output');
 
 function getData(){
@@ -18,7 +18,7 @@ function makeCard(data){
         output.innerHTML = `<img src="https://vignette.wikia.nocookie.net/internationalbroadcasts/images/5/5c/Football.png/revision/latest?cb=20140205225823" alt="goal image" height="30px" width="30px">`;
     }else{
         output.innerHTML = `
-            <p>Event Type: ${data.type}</p>
+            <p>${data.type}</p>
         `;
     };
 
